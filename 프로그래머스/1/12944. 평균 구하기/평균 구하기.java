9ff02ccb2de3a2278bdfc1249dbde214d1c25f2b
sukh115 <83396612+sukh115@users.noreply.github.com>
2026-05-13 12:47:13 +0900

    [level 1] Title: 평균 구하기, Time: 3.08 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = Arrays.stream(arr)
+                              .average()
+                              .orElse(0.0);
+        return answer;
+    }
+}
